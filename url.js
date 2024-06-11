9ce26b6baa79db2983e23b106a24571e3aea5ef2
K Vamsi Krishna <112725932+vamsiK28@users.noreply.github.com>
2024-06-11 13:40:41 +0530

    Update url.js

--- url.js
+++ url.js
@@ -1,1 +1,2 @@
-module.exports = `mongodb://localhost:27017`;
+//module.exports = `mongodb+srv://admin:admin@cluster0.vuq1vfq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+module.exports = `mongodb+srv://admin:admin@cluster0.vuq1vfq.mongodb.net/`;
