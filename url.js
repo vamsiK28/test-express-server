64e2a284431eacbbbed79508378447e93fd04bf5
K Vamsi Krishna <112725932+vamsiK28@users.noreply.github.com>
2024-06-14 11:26:55 +0530

    Update url.js

--- url.js
+++ url.js
@@ -1,2 +1,4 @@
 //module.exports = `mongodb+srv://admin:admin@cluster0.vuq1vfq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-module.exports = `mongodb+srv://admin:admin@cluster0.vuq1vfq.mongodb.net/`;
+const dbUsername = process.env.dbUsername;
+const dbPassword = process.env.dbPassword;
+let url = `mongodb+srv://${dbUsername}:${dbPassword}@cluster0.vuq1vfq.mongodb.net/capstone`;
