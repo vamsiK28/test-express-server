3a5730224975f0c8914c8bd371b04ea24027f3fb
K Vamsi Krishna <112725932+vamsiK28@users.noreply.github.com>
2024-06-14 17:06:26 +0530

    Update url.js

--- url.js
+++ url.js
@@ -2,3 +2,4 @@
 const dbUsername = process.env.dbUsername;
 const dbPassword = process.env.dbPassword;
 let url = `mongodb+srv://${dbUsername}:${dbPassword}@cluster0.vuq1vfq.mongodb.net/`;
+module.exports = url;
